5ed01dbed856cdbca3252b41542931b05046df04
Spencer Steed <info@shinespikesolutions.com>
2024-02-06 16:18:12 -0500

    new moments

--- src/assets/Answer.js
+++ src/assets/Answer.js
@@ -54,6 +54,82 @@ const gameData = [
             tournamentAnswer: 'The Big House',
             yearAnswer: 2022
         },
+
+        {
+            imagePaths: [
+                '/assets/GameImages/s2j-plup-1.png',
+                '/assets/GameImages/s2j-plup-2.png',
+                '/assets/GameImages/s2j-plup-3.png',
+            ],
+
+            gifPaths: [
+                '/assets/GameImages/s2j-plup-1.gif',
+                '/assets/GameImages/s2j-plup-2.gif',
+                '/assets/GameImages/s2j-plup-3.gif',
+            ],
+
+            
+            playerAnswers: ['s2j', 'plup'],
+            tournamentAnswer: 'Shine',
+            yearAnswer: 2022
+        },
+
+        {
+            imagePaths: [
+                '/assets/GameImages/mang0-leffen-1.png',
+                '/assets/GameImages/mang0-leffen-2.png',
+                '/assets/GameImages/mang0-leffen-3.png',
+            ],
+
+            gifPaths: [
+                '/assets/GameImages/mang0-leffen-1.gif',
+                '/assets/GameImages/mang0-leffen-2.gif',
+                '/assets/GameImages/mang0-leffen-3.gif',
+            ],
+
+            
+            playerAnswers: ['mang0', 'leffen'],
+            tournamentAnswer: 'Apex',
+            yearAnswer: 2022
+        },
+
+        {
+            imagePaths: [
+                '/assets/GameImages/armada-plup-1.png',
+                '/assets/GameImages/armada-plup-2.png',
+                '/assets/GameImages/armada-plup-3.png',
+            ],
+
+            gifPaths: [
+                '/assets/GameImages/armada-plup-1.gif',
+                '/assets/GameImages/armada-plup-2.gif',
+                '/assets/GameImages/armada-plup-3.gif',
+            ],
+
+            
+            playerAnswers: ['mang0', 'leffen'],
+            tournamentAnswer: 'Apex',
+            yearAnswer: 2022
+        },
+
+                {
+            imagePaths: [
+                '/assets/GameImages/mang0-zain-1.png',
+                '/assets/GameImages/mang0-zain-2.png',
+                '/assets/GameImages/mang0-zain-3.png',
+            ],
+
+            gifPaths: [
+                '/assets/GameImages/mang0-zain-1.gif',
+                '/assets/GameImages/mang0-zain-2.gif',
+                '/assets/GameImages/mang0-zain-3.gif',
+            ],
+
+            
+            playerAnswers: ['mang0', 'zain'],
+            tournamentAnswer: 'Summit',
+            yearAnswer: 2022
+        },
         // You can add more objects here for additional sets of images and answers
     ];
     
